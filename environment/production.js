9b1de41d32aa29df6f96f40bee9414067cc1c04e
Etudiant O'clock <thmslepine@gmail.com>
2023-05-25 19:45:06 +0200

    update

--- environment/production.js
+++ environment/production.js
@@ -3,5 +3,7 @@ const path = require('path');
 module.exports = {
   dbUrl: 'mongodb+srv://Alex:qwe@cluster0.u908xrg.mongodb.net/twitter?retryWrites=true&w=majority',
   cert: path.join( __dirname, '/etc/letsencrypt/live/www.whats-up.site/fullchain.pem'),
-  key: path.join(__dirname, '/etc/letsencrypt/live/www.whats-up.site/privkey.pem')
+  key: path.join(__dirname, '/etc/letsencrypt/live/www.whats-up.site/privkey.pem'),
+  portHttp:80,
+  portHttps:443
 }
