4e6242ef107cba5fc1a2eb72c3f4a206697c5e8a
Etudiant O'clock <thmslepine@gmail.com>
2023-05-25 18:33:21 +0200

    first

---
+++ environment/production.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+module.exports = {
+  dbUrl: 'mongodb+srv://Alex:qwe@cluster0.u908xrg.mongodb.net/twitter?retryWrites=true&w=majority',
+  cert: path.join( __dirname, ''),
+  key: path.join(__dirname, '')
+}
