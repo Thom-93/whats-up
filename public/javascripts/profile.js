8efa60676f5f2b81bf91e23a380bdeb6d6aee2e4
Thom-93 <108375234+Thom-93@users.noreply.github.com>
2023-06-16 01:56:33 +0200

    perf(profil-img): now if the user is not the same you can't change profile img and add cursor pointer

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,12 +1,12 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const inputAvatar = document.querySelector('#input-avatar');
-  const formContainer = document.querySelector('#form-container');
+window.addEventListener("DOMContentLoaded", () => {
+  const inputAvatar = document.querySelector("#input-avatar");
+  const formContainer = document.querySelector("#form-container");
 
-  formContainer.addEventListener('click', () => {
+  formContainer.addEventListener("click", () => {
     inputAvatar.click();
-  })
+  });
 
-  inputAvatar.addEventListener('change', () => {
+  inputAvatar.addEventListener("change", () => {
     formContainer.submit();
-  })
-})
+  });
+});
