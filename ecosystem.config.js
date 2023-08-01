12ca39c63ca6f4f2569ad2113ee294b0bf1fb3a2
Thom-93 <108375234+Thom-93@users.noreply.github.com>
2023-08-01 19:18:55 +0200

    fix(upload-image): try to fix with edit watch -> false

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,8 @@ module.exports = {
       script: "./bin/www",
       instances: "max",
       autorestart: true,
-      watch: true,
+      watch: "false",
+      ignore_watch: ["*.log"],
       env: {
         NODE_ENV: "development",
       },
