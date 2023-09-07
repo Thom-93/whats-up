9971928a4d9d29bbc4f4d5fd20eb475d85faca8b
Thom-93 <108375234+Thom-93@users.noreply.github.com>
2023-09-07 16:23:58 +0200

    fix(SSL)

--- environment/production.js
+++ environment/production.js
@@ -3,8 +3,8 @@ const path = require("path");
 module.exports = {
   dbUrl:
     "mongodb+srv://Alex:qwe@cluster0.u908xrg.mongodb.net/twitter?retryWrites=true&w=majority",
-  cert: path.join("/etc/letsencrypt/live/www.whats-up.site-0001/fullchain.pem"),
-  key: path.join("/etc/letsencrypt/live/www.whats-up.site-0001/privkey.pem"),
+  cert: path.join(".../etc/letsencrypt/live/www.whats-up.site/fullchain.pem"),
+  key: path.join(".../etc/letsencrypt/live/www.whats-up.site/privkey.pem"),
   portHttp: 80,
   portHttps: 443,
 };
