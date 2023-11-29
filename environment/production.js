70c8697fceda1c40df6316fd2f72c99a2bbc4228
Thom-93 <108375234+Thom-93@users.noreply.github.com>
2023-11-29 13:10:07 +0100

    Update production.js

--- environment/production.js
+++ environment/production.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   dbUrl:
-    "mongodb+srv://Alex:qwe@cluster0.u908xrg.mongodb.net/twitter?retryWrites=true&w=majority",
+    "mongodb+srv://****:***@cluster0.u908xrg.mongodb.net/twitter?retryWrites=true&w=majority",
   cert: path.join("/etc/letsencrypt/live/www.whats-up.site/fullchain.pem"),
   key: path.join("/etc/letsencrypt/live/www.whats-up.site/privkey.pem"),
   portHttp: 80,
